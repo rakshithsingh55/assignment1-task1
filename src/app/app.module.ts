d664401e14c0328d24b18f19abc98cc8e6d6ece7
rakshith <rakshithsingh0304@gmail.com>
2020-03-02 11:44:09 +0530

    commit1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { Frame1Component } from './frame1/frame1.component';
+import { Frame2Component } from './frame2/frame2.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    Frame1Component,
+    Frame2Component
   ],
   imports: [
     BrowserModule
